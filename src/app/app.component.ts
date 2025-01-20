5eea178f4d69879f00aa0946d18ff552f6666757
Bais26 <baisyufan2004@gmail.com>
2025-01-20 13:58:02 +0700

    modify app component.ts to render flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite'
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
   title = 'belajar_angular_basic';
 }
